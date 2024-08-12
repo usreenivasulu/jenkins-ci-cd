44ffca9a5818c71bbf1d82be09c8a5207be6d60b
sreenu <usreenivasulu47@gmail.com>
2024-08-12 23:18:53 +0530

    First commit

---
+++ src/main/java/com/java/tech/cloud/api/JenkinsCiCdApplication.java
@@ -0,0 +1,22 @@
+package com.java.tech.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsCiCdApplication {
+	
+	@GetMapping("/greetings/{name}")
+	public String greetings(@PathVariable String name) {
+		return "Hello "+name + "Congratuallations you have successfully completed jenkins CI CD Demo";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/tech/cloud/api/JenkinsCiCdApplicationTests.java
@@ -0,0 +1,25 @@
+package com.java.tech.cloud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class JenkinsCiCdApplicationTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testGreetingsEndpoint() throws Exception {
+		String name ="Sreenivasulu";
+		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}",name))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.content().string("Hello "+name + "Congratuallations you have successfully completed jenkins CI CD Demo"));
+		
+	}
+
+}
